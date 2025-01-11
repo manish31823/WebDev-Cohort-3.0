b837b7ebf1fb6e836501cb3fb13fad2aef15c36c
Your Name <your-email@example.com>
2025-01-11 21:49:25 +0530

    updated

---
+++ Day_2 Javascript/01.js
@@ -0,0 +1,20 @@
+// Variable-It is used to store the data
+// let,var,const
+
+// const age = 25;
+// let name = "manish"
+// var add = "sangam vihar"
+
+// Q1)Create a variable for each of the following: your favorite color, your height in 
+// centimeters, and whether you like pizza. Use appropriate variable declarations 
+// (let, const, or var). Try logging it using console.log
+
+
+const favouritecolor = ["yellow","red","blue","black"]
+const height = "172cm"
+const pizzalover = true
+
+console.log(favouritecolor);
+console.log(height);
+console.log(pizzalover);
+
