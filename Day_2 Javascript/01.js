88ed42b3fd2c7d4f7455481ff55de348ec729bc7
Your Name <your-email@example.com>
2025-01-11 23:18:12 +0530

    update

--- Day_2 Javascript/01.js
+++ Day_2 Javascript/01.js
@@ -8,3 +8,15 @@
 // Create a variable for each of the following: your favorite color, your height in
 //  centimeters, and whether you like pizza. Use appropriate variable declarations
 //   (let, const, or var). Try logging it using console.log
+
+const fcolor = ["yellow","red","green","pink"]
+const height = "172cm"
+const pizzaloverr = true
+
+console.log(fcolor);
+console.log(height);
+console.log(pizzaloverr);
+
+
+
+
