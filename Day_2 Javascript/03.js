8fe52ec3873d15523ff6516dae9ba7895ee9965c
Your Name <your-email@example.com>
2025-01-13 23:15:12 +0530

    Updated

---
+++ Day_2 Javascript/03.js
@@ -0,0 +1,15 @@
+// if-else
+// q1 Write an if/else statement that checks if a number is even or odd. If it's even, 
+// print "The number is even." Otherwise, print "The number is odd."
+// To check an even number there is an formula num%2==0
+const number =44
+if (number%2==0) {
+    console.log("It is an even number");
+}else{
+        console.log("It is an odd number");
+        
+    }
+    
+    
+
+    
