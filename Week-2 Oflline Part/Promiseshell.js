5a427f7b38361d22f8c75baecb8df3fd1cb4b6e7
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2025-01-20 22:50:11 +0530

    Add files via upload

---
+++ Week-2 Oflline Part/Promiseshell.js
@@ -0,0 +1,23 @@
+function setTimeoutPromisified(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function solve() {
+      await setTimeoutPromisified(1000);
+      console.log("hi");
+      await setTimeoutPromisified(3000);
+      console.log("hello");
+      await setTimeoutPromisified(5000);
+      console.log("hi there");
+  }
+  
+  solve();
+
+
+
+  async function main() {
+    await cleanFile("a.txt")
+    console.log("Done cleaning file");
+ }
+ 
+ main();
